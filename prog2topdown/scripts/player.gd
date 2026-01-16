712ab3418d896ff70514c316bde81267c55642bb
Miparter <miparti.real@gmail.com>
2026-01-16 08:47:37 +0100

    lade till struktur för campfire & grässbakgrund

---
+++ prog2topdown/scripts/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+@export var speed = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("key_a"):
+		velocity.x = -1 * speed 
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("running")
+	if Input.is_action_pressed("key_d"):
+		velocity.x = 1 * speed 
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("running")
+	if Input.is_action_pressed("key_w"):
+		velocity.y = -1 * speed 
+		$AnimatedSprite2D.play("running")
+	if Input.is_action_pressed("key_s"):
+		velocity.y = 1 * speed 
+		$AnimatedSprite2D.play("running")
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	else:
+		velocity = Vector2.ZERO
+		$AnimatedSprite2D.play("idle")
+	move_and_slide()
