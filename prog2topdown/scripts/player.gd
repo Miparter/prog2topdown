63ca44633ebfe648b781cf96e736c80491649ec5
Miparter <miparti.real@gmail.com>
2026-01-23 08:14:51 +0100

    tog bort fly_running

--- prog2topdown/scripts/player.gd
+++ prog2topdown/scripts/player.gd
@@ -3,23 +3,22 @@ extends CharacterBody2D
 
 @export var speed = 300.0
 
-
 func _physics_process(delta: float) -> void:
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("key_a"):
 		velocity.x = -1 * speed 
 		$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play("fly_running")
+		$AnimatedSprite2D.play("running")
 	if Input.is_action_pressed("key_d"):
 		velocity.x = 1 * speed 
 		$AnimatedSprite2D.flip_h = false
-		$AnimatedSprite2D.play("fly_running")
+		$AnimatedSprite2D.play("running")
 	if Input.is_action_pressed("key_w"):
 		velocity.y = -1 * speed 
-		$AnimatedSprite2D.play("fly_running")
+		$AnimatedSprite2D.play("running")
 	if Input.is_action_pressed("key_s"):
 		velocity.y = 1 * speed 
-		$AnimatedSprite2D.play("fly_running")
+		$AnimatedSprite2D.play("running")
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	else:
