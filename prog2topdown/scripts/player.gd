7f1752bf5fc2dca7fa9e4fa30039a5733baa5ab6
Miparter <miparti.real@gmail.com>
2026-01-29 23:57:57 +0100

    fixade rörelsen & gjorde z relativ
    fixade rörelsen genom att förstora spelet ,
    
    lade till meny musik och gjorde höjden för spelaren relativ till eldens position istället för absolut

--- prog2topdown/scripts/player.gd
+++ prog2topdown/scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var campfire := $"../Campfire"
 
 @export var speed = 250
 @onready var sprite:= $AnimatedSprite2D
@@ -7,32 +8,30 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	var input_var = Vector2.ZERO
 	
-	#fångar upp varje rörelse
+	
 	if Input.is_action_pressed("key_a"):
-		input_var.x = -1 
+		input_var.x = -1 * speed 
 		$AnimatedSprite2D.flip_h = true
 	if Input.is_action_pressed("key_d"):
-		input_var.x = 1 
+		input_var.x = 1 * speed 
 		$AnimatedSprite2D.flip_h = false
 	if Input.is_action_pressed("key_w"):
-		input_var.y = -1 
+		input_var.y = -1 * speed 
 	if Input.is_action_pressed("key_s"):
-		input_var.y = 1 
+		input_var.y = 1 * speed 
 		
-		#
+		
 	if input_var.length() > 0:
 		velocity = input_var.normalized() * speed
-		if sprite.animation !="running":
-			sprite.play("running")
+		sprite.play("running")
 	else:
 		velocity = Vector2.ZERO
-		if sprite.animation !="idle":
-			sprite.play("idle")
+		sprite.play("idle")
 		
 	move_and_slide()
 	
 	#Om spelaren är över elden, hamna bakom den
-	if self.global_position.y < 0:
+	if self.position.y < (campfire.position.y - 40):
 		z_index = 0
 	#annars var över i grafik-ordning
 	else:
