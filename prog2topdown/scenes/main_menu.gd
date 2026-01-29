9bd0a0212b9a3dfb248fdcb37f64232da040fb62
Miparter <miparti.real@gmail.com>
2026-01-30 00:44:27 +0100

    lade till musik under spelets gång och options

--- prog2topdown/scenes/main_menu.gd
+++ prog2topdown/scenes/main_menu.gd
@@ -1,9 +1,14 @@
 extends Control
 
+@onready var Song:= $menusong
+@onready var buttons: VBoxContainer = $buttons
+@onready var options: Panel = $options
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Song.play()
+	buttons.visible = true
+	options.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,8 +20,11 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	buttons.visible = false
+	options.visible = true
 
-
+func _on_options_close_pressed() -> void:
+	buttons.visible = true
+	options.visible = false
 func _on_quit_pressed() -> void:
 	get_tree().quit()
